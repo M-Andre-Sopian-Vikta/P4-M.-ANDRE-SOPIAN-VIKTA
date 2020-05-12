b004f7cb6b4c3c360212bc19710c65369f28343b
M. Andre Sopian Vikta <64565543+M-Andre-Sopian-Vikta@users.noreply.github.com>
2020-05-12 21:51:39 +0700

    Create jobsheet 9c.c
    membuat program Array sebagai karakter dengan menampilkan nama setiap mahasiswa menggunakan bahasa C

---
+++ jobsheet 9c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int i;
+char nama[5]={'A','N','D','R','E'};
+char nama2[6]="ANDRE";
+printf("Array per Karakter = ");
+for(i=0;i<=4;i++)
+{
+printf("%c",nama[i]);
+}
+printf("\n"); printf("Array string =%s\n\n",nama2);
+return 0;
+}
+
+
+
